1d9f0f06cf435e11cfbe987ee058e9666fcaec53
Axel Cho <axel@copystrong.com>
2017-07-23 12:05:43 -0400

    change port to heroku standard

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 
+app.set('port', (process.env.PORT || 5000));
+
 router.use(function (req,res,next) {
     console.log("/" + req.method);
     next();
@@ -48,6 +50,6 @@ app.use("*",function(req,res){
     res.sendFile(path + "404.html");
 });
 
-app.listen(3000,function(){
-    console.log("Live at Port 3000");
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
